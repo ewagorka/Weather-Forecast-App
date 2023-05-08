af5908851e6c77f788ac9bd4ac18eebc4baf5bcb
ewagorka <ewagorka99@gmail.com>
2023-05-08 22:31:40 +0100

    Added save history functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ console.log(today);
 
 var results;
 
+var searches=[];
+
+renderSearches();
 //when search button is clicked
 $("#search-button").on("click", function (event) {
     //prevent default
@@ -20,61 +23,119 @@ $("#search-button").on("click", function (event) {
         method: "GET"
     })
         .then(function (response) {
-            //store response from query
-            results = response;
-            console.log(results);
-
-            //get city name
-            var city = response.city.name;
-            //get today's most recent data
-            var todayData = response.list[0];
-
-            //Display heading
-            $("#today").children("h2").text(city +" "+today.format("DD/MM/YYYY"));
-            $("#today").children("img").attr("src","https://openweathermap.org/img/wn/"+todayData.weather[0].icon+"@2x.png");
-
-            //Display weather data
-            $("#today").children("p").html(
-                "Temp: "+(todayData.main.temp).toFixed(1)+"°C"+"<br>"+
-                "Wind: "+todayData.wind.speed+" KPH"+"<br>"+
-                "Humidty: "+todayData.main.humidity+"%");
-            //Todays weather forecast
-
-            //for each card in the 5 day forecast area
-            for (var i = 0; i < 5; i++) {
-                
-                //create new div, add bootstrap class
-                var cardDiv = $("<div>").attr("class", "card card-body");
-
-
-                //grab todays date at each iteration
-                today = moment();
-                //get headline
-                cardDate = today.add(i+1,"days"); 
-                var headline = $("<h5>").text(cardDate.format("DD/MM/YYYY"));
-                
-                // search through the list array for the entries that store data for the next day at 15:00
-                var cardDateData = response.list.find(function(data){
-                    return cardDate.format("YYYY-MM-DD")+" 15:00:00"==data.dt_txt;
-                })
-
-                //add weather icon
-                var cardImage =$("<img>").attr("src","https://openweathermap.org/img/wn/"+cardDateData.weather[0].icon+"@2x.png");
-                
-                //add weather info
-                var p = $("<p>").html(
-                    "Temp: "+(cardDateData.main.temp).toFixed(1)+"°C"+"<br>"+
-                    "Wind: "+cardDateData.wind.speed+" KPH"+"<br>"+
-                    "Humidty: "+cardDateData.main.humidity+"%");
-
-                //append all elements to card div
-                cardDiv.append(headline, cardImage, p);
-                //append card div to forecast area
-                $("#forecast").append(cardDiv);
-            }
+            displayForecast(response);
+            saveSearch(qEl);
         })
         //Display error message
         .catch(err =>
             alert("Something went wrong, search for a city again")
+        );
+});
+
+function displayForecast(response){
+    //store response from query
+    results = response;
+    console.log(results);
+
+    //get city name
+    var city = response.city.name;
+    //get today's most recent data
+    var todayData = response.list[0];
+
+    //Display heading
+    $("#today").children("h2").text(city +" "+today.format("DD/MM/YYYY"));
+    $("#today").children("img").attr("src","https://openweathermap.org/img/wn/"+todayData.weather[0].icon+"@2x.png");
+
+    //Display weather data
+    $("#today").children("p").html(
+        "Temp: "+(todayData.main.temp).toFixed(1)+"°C"+"<br>"+
+        "Wind: "+todayData.wind.speed+" KPH"+"<br>"+
+        "Humidty: "+todayData.main.humidity+"%");
+    //Todays weather forecast
+
+    //for each card in the 5 day forecast area
+    for (var i = 0; i < 5; i++) {
+        
+        //create new div, add bootstrap class
+        var cardDiv = $("<div>").attr("class", "card card-body");
+
+
+        //grab todays date at each iteration
+        today = moment();
+        //get headline
+        cardDate = today.add(i+1,"days"); 
+        var headline = $("<h5>").text(cardDate.format("DD/MM/YYYY"));
+        
+        // search through the list array for the entries that store data for the next day at 15:00
+        var cardDateData = response.list.find(function(data){
+            return cardDate.format("YYYY-MM-DD")+" 15:00:00"==data.dt_txt;
+        })
+
+        //add weather icon
+        var cardImage =$("<img>").attr("src","https://openweathermap.org/img/wn/"+cardDateData.weather[0].icon+"@2x.png");
+        
+        //add weather info
+        var p = $("<p>").html(
+            "Temp: "+(cardDateData.main.temp).toFixed(1)+"°C"+"<br>"+
+            "Wind: "+cardDateData.wind.speed+" KPH"+"<br>"+
+            "Humidty: "+cardDateData.main.humidity+"%");
+
+        //append all elements to card div
+        cardDiv.append(headline, cardImage, p);
+        //append card div to forecast area
+        $("#forecast").append(cardDiv);
+    }
+
+}
+//Save search and render new button
+function saveSearch(value){
+
+    var button = $("<button>")
+    button.text(value).attr("class","btn")
+
+    $("#history").append(button);
+
+    searches.push(value);
+    localStorage.setItem("savedSearches",JSON.stringify(searches))
+
+}
+
+//Render all saved searches as buttons
+function renderSearches(){
+    var savedSearches = JSON.parse(localStorage.getItem("savedSearches"))
+
+    console.log(savedSearches);
+    if(savedSearches!= null){
+        console.log("hey");
+        searches =savedSearches;
+    }
+
+    for(i =0; i<searches.length;i++){
+        var button = $("<button>")
+        button.text(searches[i]).attr("class","btn savedBtn")
+
+        $("#history").append(button);
+    }
+}
+
+$(document).on("click",".savedBtn",function(event){
+    event.preventDefault();
+    //empty forecast area
+    $("#forecast").empty();
+
+    //get value from input
+    var qEl = $(this).text().trim();
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + qEl + "&units=metric&appid=88c1c3f53bd4ff31a0846bec366c6e7c";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            displayForecast(response);
+        })
+        //Display error message
+        .catch(err =>
+            alert("Something went wrong, search for a city again h")
         );
 });
